d82b3d7843ffcd356b46e3ee17305405d79df8bc
Majed Al-Shuwaiee <m.shuwaiee@gmail.com>
2025-04-17 17:26:55 +0300

    Initial commit

---
+++ src/main/kotlin/com/shuwaiee/classroomb/ClassRoomBApplication.kt
@@ -0,0 +1,11 @@
+package com.shuwaiee.classroomb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClassRoomBApplication
+
+fun main(args: Array<String>) {
+	runApplication<ClassRoomBApplication>(*args)
+}
---
+++ src/main/kotlin/com/shuwaiee/classroomb/UserController.kt
@@ -0,0 +1,4 @@
+package com.shuwaiee.classroomb
+
+class UserController {
+}
